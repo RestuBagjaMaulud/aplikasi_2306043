93851e122d8b94ec8a6e915c75e191a412df4d64
Restu_Bagja_Maulud <2306043@itg.ac.id>
2026-03-11 11:29:40 +0700

    Update project latihan 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,195 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(HomePage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    String title = 'Aplikasi Pertamaku';
-    String name = 'Restu Bagja Maulud';
-    String message = 'Halo nama saya $name, selamat datang di $title';
-    
+class HomePage extends StatelessWidget{
+  build(context){
     return MaterialApp(
-      title: title,
       home: Scaffold(
-        appBar: AppBar(backgroundColor: Colors.cyan,
-          title: Text(title),
+        appBar: AppBar(
+          backgroundColor: Colors.red[800],
+          leading: Icon(Icons.home),
+          title: Text('Aplikasi Pertamaku'),
         ),
-        body: Column(
-          children: [
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Padding(padding: EdgeInsets.all(10.0),
-            child: Text(message))
-          ],
+        body: SingleChildScrollView(
+          child: Container(
+          margin: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Row(
+                children: [
+              Icon(Icons.archive),
+              Text("Artikel Terbaru", style: TextStyle(fontWeight: .bold),)
+                ],
+              ),
+              Card(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text("Teknologi AI: Revolusi Kecerdasan Buatan yang Mengubah Dunia", style: TextStyle(fontSize: 30, fontWeight: .bold, ),),
+                    Image.network("https://tse4.mm.bing.net/th/id/OIP.AUTrbjzBq8rpKgH5YDznbAHaEK?pid=Api&P=0&h=180",
+                    height: 300,
+                    fit: BoxFit.cover,),
+                    Text("AI atau Artificial Intelligence merupakan teknologi yang sedang berkembang pesat di Indonesia. Dengan kemampuan untuk memproses data dalam jumlah besar dan belajar dari pengalaman, AI dapat digunakan dalam berbagai bidang seperti kesehatan, transportasi, dan pendidikan. Di Indonesia, banyak perusahaan dan institusi yang mulai mengadopsi teknologi AI untuk meningkatkan efisiensi dan inovasi dalam bisnis mereka. Namun, tantangan seperti kurangnya sumber daya manusia yang terampil dan masalah etika juga perlu diatasi untuk memastikan perkembangan AI yang berkelanjutan di Indonesia.")
+                  ],
+                ),
+              ),
+              SizedBox(height: 20,),
+              Row(
+                children: [
+                  Text("Komentar",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold
+                    ),
+                  )
+                ],
+              ),
+              ListView(
+                shrinkWrap: true,
+                children: [
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text("Restu Bagja Maulud"),
+                          Text("Artikelnya menarik sekali!")
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text("Muhammad Ar Rasyid Rizki Oktavian"),
+                          Text("Artikelnya bagus sekali!")
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text("Irham Sugriantha"),
+                          Text("Artikelnya Garing sekali!")
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text("Muhammad Fahmi Faisal"),
+                          Text("Artikelnya Simpel sekali!")
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text("Restu QuinzeL"),
+                          Text("Artikelnya menarik sekali!")
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text("Restu QuinzeL"),
+                          Text("Artikelnya menarik sekali!")
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text("Restu QuinzeL"),
+                          Text("Artikelnya menarik sekali!")
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text("Restu QuinzeL"),
+                          Text("Artikelnya menarik sekali!")
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text("Restu QuinzeL"),
+                          Text("Artikelnya menarik sekali!")
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text("Restu QuinzeL"),
+                          Text("Artikelnya menarik sekali!")
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text("Restu QuinzeL"),
+                          Text("Artikelnya menarik sekali!")
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
+        )
+        
       ),
     );
   }
 }
-      
